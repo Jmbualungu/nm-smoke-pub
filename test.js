20d0a9cff92753355a08191b31c8616f99a39a6d
NM Test <test@example.com>
2026-06-28 19:37:25 -0400

    feat: add multiply()

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 const assert = require('assert');
-const { add } = require('./math.js');
+const { add, multiply } = require('./math.js');
 
 assert.strictEqual(add(2, 3), 5);
+assert.strictEqual(multiply(2, 3), 6);
 console.log('all tests passed');
